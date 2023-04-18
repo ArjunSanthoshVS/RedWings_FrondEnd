1faf493481e8802d7681a5e030b31232f6cdbc51
Arjun Santhosh <arjunsanthosh738@gmail.com>
2023-04-18 18:34:33 +0530

    3 commit

--- src/Redux/api.js
+++ src/Redux/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://redwings-backend.onrender.com" })
+const API = axios.create({ baseURL: "https://redwings-backend.onrender.com/api" })
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem("userToken")) {
